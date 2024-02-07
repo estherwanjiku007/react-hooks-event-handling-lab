41445cf90ae4431b4cecc07ec59add04b8bad96c
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-02-07 15:59:07 +0300

    aster

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(event){
+    function EyesOnMe2(){
+       console.log("Good!")
+        }
+    function EyesOnMe3(){
+        console.log("Hey! Eyes on me!")
+    }
+    if (event===onfocus){    
+    return (
+        <div>
+            <button onFocus={EyesOnMe2}>Eyes on me</button>
+            </div>
+    )}
+    else{
+        return( <div>
+            <button onBlur={EyesOnMe3}>Eyes on me</button> 
+        </div>)
+    }
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad ({event}){
+    function Keypad2(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={Keypad2}/>
+        </div>
     )
 }
 
